db5ffe83d00bf1ace93060091e61947929052a4b
annemariemygind <annro@itu.dk>
2021-12-07 13:18:22 +0100

    Test

--- src/main/java/com/example/eksamensprojekt/MyListController.java
+++ src/main/java/com/example/eksamensprojekt/MyListController.java
@@ -1,7 +1,30 @@
 package com.example.eksamensprojekt;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MyListController {
+    
+    @FXML
+    public void goToFilmList(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FilmList.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
+    //Vis tilføjede medier
+    //Metode - Fjern tilføjede medier
+
+    //Skal man tilføje medier fra FilmList og SerieList?
 
 }
