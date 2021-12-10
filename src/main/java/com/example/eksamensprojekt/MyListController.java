9992036f7957d3d858a6649dbeeddaf78967de1c
annemariemygind <annro@itu.dk>
2021-12-10 19:31:39 +0100

    Mylist - Tester initialize

--- src/main/java/com/example/eksamensprojekt/MyListController.java
+++ src/main/java/com/example/eksamensprojekt/MyListController.java
@@ -3,15 +3,28 @@ package com.example.eksamensprojekt;
 import com.example.eksamensprojekt.Models.Film;
 import com.example.eksamensprojekt.Services.LoadingFilm;
 import com.example.eksamensprojekt.Services.MyList;
+import com.example.eksamensprojekt.Services.Search;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,18 +81,59 @@ public class MyListController {
     }
 
     @FXML
-    public void renderFilm(List<Film> film) {
+    private GridPane MyListfilmGridPane;
+
+    @FXML
+    public void renderMyListFilm(ArrayList<Film> film)
+    {
         int i = 0;
         for (Film f : film) {
-            
-        }
+            //Laver parametrer til labels
+            Label titleLabel = new Label(f.getName());
+            Label yearLabel = new Label(f.getYear() + "");
+            Label genreToStringLabel = new Label(f.genreToString() + "");
+            Label ratingLabel = new Label(f.getRating() + "");
+            Button btn = new Button("Play Film");
+
+            //Henter image/thumbnail
+            URL url = MyListController.class.getResource(f.getImage());
+            Image image = new Image(String.valueOf(url));
+            ImageView thumbnailImageView = new ImageView(image);
+
+            ratingLabel.setPadding(new Insets(0, 0, 1, 0));
+
+            //Laver en virtuel box i hvert rum i gridpane, som smider alle labels ind i rækkefølge
+            VBox box = new VBox(titleLabel, yearLabel, genreToStringLabel, ratingLabel, thumbnailImageView, btn);
+            box.setAlignment(Pos.BASELINE_CENTER);
+            box.setPadding(new Insets(12, 12, 12, 12));
+
+            MyListfilmGridPane.add(box, i % 3, Math.floorDiv(i, 3)); //gør at hver række går fra index 0 til 2 og floor gør at der divideres med 3 fra listen, tager den heltal lavest
+            i++;
+
+            btn.setOnMouseClicked((event) -> {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("Playwindow.fxml"));
+
+                    Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+                    Stage stage = new Stage();
+                    stage.setTitle("Movie playing");
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }}
+
+    @FXML
+    //test initialize
+    public void initialize() throws IOException {
+        MyList ml = new MyList(); //instantierer mylist
+        LoadingFilm lf = new LoadingFilm();  //-II- loadingfilm
+        List<Film> film = lf.openFile(); //fylder film arrayliste
+        ArrayList AList = ml.mylistFilm;
+        ml.findLoadListFilm("testl", AList);
+        //renderMyListFilm(AList);
     }
-
-   /* @FXML
-    public void initialize(){
-
-    } */
-
-
-
 }
