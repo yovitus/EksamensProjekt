307e1550fd497697880f61395ba7c6ab870f099f
Elisabeth <elbr@itu.dk>
2021-12-10 11:43:28 +0100

    t

---
+++ src/main/java/com/example/eksamensprojekt/Models/Episode.java
@@ -0,0 +1,9 @@
+package com.example.eksamensprojekt.Models;
+
+public class Episode {
+    public String[] episodes;
+
+    public Episode(String[] episodes) {
+        this.episodes = episodes;
+    }
+}
