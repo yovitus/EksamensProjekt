c7d683584f90e3120cc3af9c8973f01bc7b18226
annemariemygind <annro@itu.dk>
2021-12-13 15:35:21 +0100

    Slettet ubrugte klasser & imports

--- src/main/java/com/example/eksamensprojekt/Models/Episode.java
+++
@@ -1,9 +0,0 @@
-package com.example.eksamensprojekt.Models;
-
-public class Episode {
-    public String[] episodes;
-
-    public Episode(String[] episodes) {
-        this.episodes = episodes;
-    }
-}
