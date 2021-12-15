a47dbc7f210a01ad6c2542087dc03e7bd8d18362
yovitus <vime@itu.dk>
2021-12-15 12:56:41 +0100

    Har lavet en exceptin hvis username findes

--- src/main/java/com/example/eksamensprojekt/Services/LoadingFilm.java
+++ src/main/java/com/example/eksamensprojekt/Services/LoadingFilm.java
@@ -48,11 +48,6 @@ public class LoadingFilm {
         return film;
     }
 
-    public void closeFile()
-    {
-        s.close();
-    }
-
     public List<Film> getFilm() {
         return film;
     }
